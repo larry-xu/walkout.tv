bd63e1342fa7af101d6cc4189c0d7384cab9c419
Larry Xu <larry@larryxu.com>
2020-06-12 02:27:53 -0700

    Add dev server commands

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,9 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: "development",
   devtool: "source-map",
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new webpack.DefinePlugin({
       CLIENT_DOMAIN: JSON.stringify("localhost"),
